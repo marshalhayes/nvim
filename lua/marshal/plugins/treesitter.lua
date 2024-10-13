de08e6f2a35a0a4d9f24bb5b9d3b0926158e7155
Marshal Hayes <17213165+marshalhayes@users.noreply.github.com>
2024-10-13 11:35:28 -0500

    fix lsp errors

--- lua/marshal/plugins/treesitter.lua
+++ lua/marshal/plugins/treesitter.lua
@@ -1,19 +1,34 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup {
-      ensure_installed = { "html", "css", "javascript", "typescript", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"html",
+				"css",
+				"javascript",
+				"typescript",
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"markdown",
+				"markdown_inline",
+			},
 
-      -- Automatically install languages as they're needed
-      auto_install = true,
+			-- Automatically install languages as they're needed
+			auto_install = true,
 
-      highlight = {
-        enable = true,
+			highlight = {
+				enable = true,
 
-        -- Don't use regex for highlighting
-        additional_vim_regex_highlighting = false,
-      }
-    }
-  end
+				-- Don't use regex for highlighting
+				additional_vim_regex_highlighting = false,
+			},
+
+			modules = {},
+			sync_install = false,
+			ignore_install = {},
+		})
+	end,
 }
