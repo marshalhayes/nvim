f46c26923664560626faa80f054086ff1c20132e
Marshal Hayes <17213165+marshalhayes@users.noreply.github.com>
2024-10-13 11:37:36 -0500

    format

--- lua/marshal/lazy.lua
+++ lua/marshal/lazy.lua
@@ -4,21 +4,21 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
 
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
 
-    vim.fn.getchar()
+		vim.fn.getchar()
 
-    os.exit(1)
-  end
+		os.exit(1)
+	end
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -26,15 +26,15 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	-- Import plugins from the plugins folder
 	spec = {
-		{ import = "marshal/plugins" }
+		{ import = "marshal/plugins" },
 	},
 
 	-- Automatically check for plugin updates
 	checker = { enabled = true },
 
-  -- Perform change detection, but don't tell me about it
-  change_detection = {
-    enabled = true,
-    notify = false
-  }
+	-- Perform change detection, but don't tell me about it
+	change_detection = {
+		enabled = true,
+		notify = false,
+	},
 })
