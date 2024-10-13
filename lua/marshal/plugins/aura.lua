af03092e99fac3a9a8bd12ca340c58a31194f5e7
Marshal Hayes <17213165+marshalhayes@users.noreply.github.com>
2024-10-13 11:29:26 -0500

    use transparent background on floating windows

--- lua/marshal/plugins/aura.lua
+++ lua/marshal/plugins/aura.lua
@@ -1,19 +1,23 @@
 -- This file is used to configure the horizon theme and the nvim-treesitter plugin
 return {
-  { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
 
-  { 'baliestri/aura-theme',
-    lazy = false,
-    priority = 1000,
-    config = function(plugin)
-      vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
-      vim.cmd([[colorscheme aura-dark]])
+	{
+		"baliestri/aura-theme",
+		lazy = false,
+		priority = 1000,
+		config = function(plugin)
+			vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
+			vim.cmd([[colorscheme aura-dark]])
 
-      -- Tell vim this a dark background
-      vim.o.background = "dark"
+			-- Tell vim this a dark background
+			vim.o.background = "dark"
 
-      -- Tell vim to use a transparent background
-      vim.cmd([[highlight Normal guibg=NONE ctermbg=NONE]])
-    end
-  }
+			-- Tell vim to use a transparent background
+			vim.cmd([[highlight Normal guibg=NONE ctermbg=NONE]])
+
+			-- Tell vim to use a transparent background on float windows
+			vim.cmd([[highlight NormalFloat guibg=NONE ctermbg=NONE]])
+		end,
+	},
 }
