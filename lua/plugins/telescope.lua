4764e57df29dd558edc1bd7977d023e573c869e5
Marshal Hayes <17213165+marshalhayes@users.noreply.github.com>
2024-10-28 21:39:06 -0500

    add more telescope keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,5 +12,24 @@ return {
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [f]iles" })
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[F]ind by [g]rep" })
 		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+
+		vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "[F]ind [w]ord" })
+
+		vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "[F]ind [r]eferences" })
+
+		vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "[F]ind [k]eymaps" })
+
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [h]elp" })
+
+		vim.keymap.set("n", "<leader>/", function()
+			builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+				winblend = 10,
+				previewer = false,
+			}))
+		end, { desc = "[/] Find in current buffer" })
+
+		vim.keymap.set("n", "<leader>fn", function()
+			builtin.find_files({ cwd = vim.fn.stdpath("config") })
+		end, { desc = "[F]ind [n]vim files" })
 	end,
 }
