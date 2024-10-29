81c98f0d752291f5f125f7be062237568de4fff6
Marshal Hayes <17213165+marshalhayes@users.noreply.github.com>
2024-10-28 22:07:20 -0500

    add folding with treesitter

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,6 @@
 -- Set the map leader
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- Use undo files
 vim.opt.undofile = true
@@ -16,26 +16,33 @@ vim.opt.shiftwidth = 2
 vim.opt.number = true
 
 -- Enable mouse
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 
 -- Case-insensitive search unless a capital letter is used
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 -- Keep the sign column
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 
 -- Show which line the cursor is on
 vim.opt.cursorline = true
 
 -- Clear search highlights after hitting escape
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Window navigation
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 -- Displays which-key popup sooner
 vim.opt.timeoutlen = 300
+
+-- Set the fold mode
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+
+-- Don't fold on startup
+vim.opt.foldenable = false
